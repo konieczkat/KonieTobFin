6c30d3dc4570dbe87ca10cbb2a9b744e751911fa
konieczkat <konieczkat@outlook.com>
2023-10-04 12:22:03 -0400

    Update visualyze_line.R

--- R/visualyze_line.R
+++ R/visualyze_line.R
@@ -13,7 +13,7 @@
 visualyze_line <- function(data, dateBreaks = waiver(), dateLabels = waiver()){
   aPlot <- ggplot(data, aes(x = date, y = people_at_least_one_dose)) +
     geom_line() +
-    labs(y = "People with at Least One Dose", x = "Date") +
+    labs(y = "People with One or More Doses", x = "Date") +
     ggtitle("Vaccination Trend") + scale_x_date(date_breaks = dateBreaks, date_labels = dateLabels)
   return(aPlot)
 }
