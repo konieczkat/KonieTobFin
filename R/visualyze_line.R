6a99e8f23674daa966bdee4b3630c71a8aa46829
konieczkat <konieczkat@outlook.com>
2023-10-04 17:43:31 -0400

    generalized visualyze_line

--- R/visualyze_line.R
+++ R/visualyze_line.R
@@ -5,13 +5,14 @@
 #' @import dplyr
 #'
 #' @param data The tibble from which the plot will be constructed
+#' @param interest A vector of information to be plotted against time
 #' @param dateBreaks Specifies the data breaks in the x-axis
 #' @param dateLabels Specifies the data Labels in the x-axis
 #'
 #' @export
 #'
-visualyze_line <- function(data, dateBreaks = waiver(), dateLabels = waiver()){
-  aPlot <- ggplot(data, aes(x = date, y = people_at_least_one_dose)) +
+visualyze_line <- function(data, interest, dateBreaks = waiver(), dateLabels = waiver()){
+  aPlot <- ggplot(data, aes(x = date, y = interest)) +
     geom_line() +
     labs(y = "People with One or More Doses", x = "Date") +
     ggtitle("Vaccination Trend") + scale_x_date(date_breaks = dateBreaks, date_labels = dateLabels)
