9a6917aba772b864ffc291e3e875033716e6d70a
konieczkat <konieczkat@outlook.com>
2023-10-03 22:11:40 -0400

    changes

--- R/vax_percentage.R
+++ R/vax_percentage.R
@@ -1,7 +1,10 @@
 #' Finds the percentage of vaccinated people on a given day.
-#'
+#' @import tidyverse
+#' @import dplyr
 #' @export
-#'
+
 vax_percentage <- function(data){
-  return(data$Percent_Vax <- (data$people_at_least_one_dose / data$population)*100)
+  percentage <- (data$at_least_one_dose / data$population) * 100
+  newData <- mutate(data, percentage)
+  return(newData)
 }
