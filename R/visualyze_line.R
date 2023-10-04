2f76fd311abf30b0ab601af2e65f7236de88cf72
konieczkat <konieczkat@outlook.com>
2023-10-04 11:48:42 -0400

    generalizing code

--- R/visualyze_line.R
+++ R/visualyze_line.R
@@ -6,10 +6,10 @@
 #'
 #' @export
 #'
-visualyze_line <- function(data, breaks){
+visualyze_line <- function(data, dateBreaks = waiver(), dateLabels = waiver()){
   aPlot <- ggplot(data, aes(x = date, y = people_at_least_one_dose)) +
     geom_line() +
     labs(y = "People with at Least One Dose", x = "Date") +
-    ggtitle("Vaccination Trend") + scale_x_date(date_breaks = breaks, date_labels = "%b %Y")
+    ggtitle("Vaccination Trend") + scale_x_date(date_breaks = dateBreaks, date_labels = dateLabels)
   return(aPlot)
 }
